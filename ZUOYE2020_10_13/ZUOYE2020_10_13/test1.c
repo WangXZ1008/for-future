857b2820b1bd30f52383ba7f5e5b89b12043fd63
WangXZ1008 <740150178@qq.com>
2020-10-28 22:35:28 +0800

    提交代码

---
+++ ZUOYE2020_10_13/ZUOYE2020_10_13/test1.c
@@ -0,0 +1,75 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <Windows.h>
+
+
+int main(){
+
+
+	
+	
+	//写一个代码打印1-100之间所有3的倍数的数字
+	//int i;
+	//for (i = 1; i < 101; i++) {
+	//	Sleep(100);//控制输出速度
+	//	if (i%3==0){
+	//		printf("%d\n", i);
+	//	}
+	//	else{
+	//		
+	//	}
+	//}
+	
+	
+	//写一个代码将三个数按从大到小输出
+	//在abc后写三个要进行比较的数
+	/*int a = ;
+	int b = ;
+	int c = ;
+	if (a<b)
+	{
+		int tmp = a;
+		a = b;
+		b = tmp;
+	}
+	if (a<c)
+	{
+		int tmp = a;
+		a = c;
+		c = tmp;
+	}
+	if (b<c)
+	{
+		int tmp = b;
+		b = c;
+		c = tmp;
+	}
+	printf("a=%d b=%d c=%d\n", a, b, c);*/
+	//写一个代码将三个数按从大到小输出
+	/*int num1 = 0;
+	int num2 = 0;
+	int num3 = 0;
+	printf("请输入三个数\n");
+	scanf("%d %d %d", &num1, &num2, &num3);
+	if (num1 > num2 > num3) {
+		printf("%d %d %d\n",num1,num2,num3);
+	}
+	else if (num1 > num3 > num2){
+		printf("%d %d %d\n",num1,num3,num2);
+	}
+	else if (num2 > num1 > num3){
+		printf("%d %d %d\n",num2,num1,num3);
+	}
+	else if (num2 > num3 > num1){
+		printf("%d %d %d\n",num2,num3,num1);
+	}
+	else if (num3 > num1 > num2){
+		printf("%d %d %d\n",num3,num1,num2);
+	}
+	else (num3 > num2 > num1); {
+		printf("%d %d %d\n",num3,num2,num1);
+	}*/
+
+	system("pause");
+	return 0;
+}
